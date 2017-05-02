c5a6dca34b5687c121143e048ddd16c66c64b5d6
Mark van Straten <mark@q42.nl>
2017-05-02 10:21:32 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# statsd-daemon
-Simple StatsD server to observe metrics being emitted
+# Statsd Daemon
+
+This repository contains a simple statsd UDP server. When you run it on your local machine any other  application emitting StatsD metrics to localhost will end up on the console output of this statsd-deamon.
+
+Handy if you want to verify if your metrics are what you expect them to be.
+
+NOTE: this is by no means a full-fledged statsd aggregator.
+
+# run
+npm start
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+const PORT = 8125;
+const HOST = '127.0.0.1';
+
+server.on('listening', () => {
+  const address = server.address();
+  // eslint-disable-next-line no-console
+  console.log(`UDP Server listening on ${address.address}:${address.port}`);
+});
+
+server.on('message', (message, remote) => {
+  // eslint-disable-next-line no-console
+  console.log(`${remote.address}:${remote.port} - ${message}`);
+});
+
+server.bind(PORT, HOST);
