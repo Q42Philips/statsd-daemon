8e77ef3859dd7e57663e073a6fdf31063103a21f
Herman <herman@q42.nl>
2019-10-11 09:41:42 +0200

    Allow listening on other interfaces
    This is necessary when running in Docker: need to listen on 0.0.0.0.

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const server = dgram.createSocket('udp4');
 
 const chalk = require('chalk');
 
-const PORT = 8125;
-const HOST = '127.0.0.1';
+const PORT = process.env.LISTEN_PORT ? parseInt(process.env.LISTEN_PORT, 10) : 8125;
+const HOST = process.env.LISTEN_HOST || '127.0.0.1';
 
 server.on('listening', () => {
   const address = server.address();
