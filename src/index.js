fb910166d859bf85b2eb7ed221a100024bca6086
Herman Banken <hermanbanken@gmail.com>
2019-10-16 11:12:15 +0200

    Support JSON logging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+/* eslint-disable no-console */
+
 const updateNotifier = require('update-notifier');
 /* Notify if an update is available */
 updateNotifier({ pkg: require('../package.json') }).notify();
@@ -11,17 +13,24 @@ const chalk = require('chalk');
 
 const PORT = process.env.LISTEN_PORT ? parseInt(process.env.LISTEN_PORT, 10) : 8125;
 const HOST = process.env.LISTEN_HOST || '127.0.0.1';
+const logformat = process.env.LOGFORMAT || 'text';
 
 server.on('listening', () => {
   const address = server.address();
-  // eslint-disable-next-line no-console
-  console.log(`UDP Server listening on ${address.address}:${address.port}`);
+  if (logformat === 'json') {
+    console.log(JSON.stringify({ eventTime: new Date().toISOString(), message: `UDP Server listening on ${address.address}:${address.port}`, severity: "DEBUG" }));
+  } else {
+    console.log(`UDP Server listening on ${address.address}:${address.port}`);
+  }
 });
 
 server.on('message', (message, remote) => {
   const [keyval, datatype, tags] = message.toString('utf8').split('|');
-  // eslint-disable-next-line no-console
-  console.log(chalk.cyan(new Date().toTimeString()) + '\t' + chalk.green(keyval) + '(' + chalk.italic.red(datatype) + ')\t' + chalk.italic.yellow(tags));
+  if (logformat === 'json') {
+    console.log(JSON.stringify({ eventTime: new Date().toISOString(), message: `${keyval}(${datatype})\t${tags}`, severity: "INFO" }));
+  } else {
+    console.log(chalk.cyan(new Date().toTimeString()) + '\t' + chalk.green(keyval) + '(' + chalk.italic.red(datatype) + ')\t' + chalk.italic.yellow(tags));
+  }
 });
 
 server.bind(PORT, HOST);
